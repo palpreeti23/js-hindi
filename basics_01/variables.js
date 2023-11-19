04d59913c36e53c689f27d94cf19d328555352f3
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2023-11-19 06:57:08 +0000

    variables

---
+++ basics_01/variables.js
@@ -0,0 +1,20 @@
+const name = "preti pal";
+let age = 20;
+var emailId = "preeti12@.com";
+school = "st joseph school";
+let education;
+
+// let name = "neha pal"; // const can't be changed
+
+ age = 18;
+var emailId = 'pal232@.com';
+school = "bharat mata school";
+/*
+try using let and const mostly const instead of var 
+bcoz of issues in block scope and functional scope
+
+defining a variable without const and let works but not actually a goood practice.
+*/
+
+console.log(name);
+console.log([name, age, school,emailId,education])
