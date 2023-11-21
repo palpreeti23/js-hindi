b7ef366adf54285a5c57ab55f0e4412bc660ee83
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2023-11-21 17:06:30 +0000

    conversion of datatypes

---
+++ basics_01/conversionOperation.js
@@ -0,0 +1,25 @@
+let name = "preeti";
+// console.log(name);
+// console.log(typeof (name))
+
+let num = null;
+let stringNum = Number(num)
+console.log(stringNum);
+console.log(typeof (stringNum))
+
+// '33swdd' => NaN
+//'33' => 33
+
+
+let loggedIn = 0;
+let booleanLoggedIn = Boolean(loggedIn)
+// console.log(booleanLoggedIn)
+// console.log(typeof booleanLoggedIn)
+
+// 1 => true && 0 => false
+// "preeti" => true && "" => false
+
+let num1 = 33
+let isString = String(num)
+// console.log(isString);
+// console.log(typeof isString)
---
+++ basics_01/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //this treats all the js code in newer version 
+
+// alert("preeti pal")
+
+//we can not use alert in node js but in browser
+
+/* 
+some basic datatypes in js
+
+Number => 2 to power 53
+bigInt => for larger Number
+String => " "
+Boolean => true/false
+null => standalon value //null is an object
+let state = null;
+
+undefined => undefined type
+let dist;
+
+symbol => unique value
+
+*/
+
+console.log(typeof null); //null is an object
+
+console.log(typeof undefined);
