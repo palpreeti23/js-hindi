aa4f7f53890e1206e04f6f5e8586bd609ed7f226
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2023-11-26 15:19:02 +0000

    numbers and methods

---
+++ basics_01/nums-and-maths.js
@@ -0,0 +1,33 @@
+const score = 300;
+
+const newScore = new Number(500)
+
+// console.log(newScore);
+// console.log(newScore.toString().length);
+// console.log(newScore.valueOf());
+
+const balance = 500.8457777;
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+const largeNumber = 1000000;
+// console.log(largeNumber.toLocaleString('en-IN'));
+
+
+/*    -------------------- MATHs-------------------        */
+
+// console.log(Math.abs(-4)) // change the neg value into positive
+// console.log(Math.round(6.67));
+// console.log(Math.ceil(4.388));
+// console.log(Math.floor(34.5366));
+// console.log(Math.min(3,6,8,4,1,9));
+// console.log(Math.max(3,6,8,4,1,9));
+
+
+console.log(Math.random())
+console.log((Math.round((Math.random()*10)+1)));
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor((Math.random()*(max-min + 1))+min))
