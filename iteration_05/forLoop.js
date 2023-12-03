e13cdb69f0f43527e72f8c04a22a51a54c58cb8c
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2023-12-03 17:04:48 +0000

    basic loops done

---
+++ iteration_05/forLoop.js
@@ -0,0 +1,42 @@
+// for (let index = 0; index < 10; index++) { 
+//     console.log(index)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`the value of i is ${i}`);
+//     for(let j = 0; j<=10; j++){
+//         console.log(`i*j = ${i*j}`);
+//     }
+// }
+
+// let myArr = ["jyoti","preeti","priya","neha"];
+// console.log(myArr.length);
+
+// for (let i = 0; i < myArr.length; i++) {
+//     const element = myArr[i];
+//     console.log(element)
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+  
+//     if(i == 5){
+//         console.log("5 detected");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+for (let i = 0; i <= 10; i++) {
+  
+    if(i == 5){
+        console.log("5 detected");
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+}
+
+
---
+++ iteration_05/whileLoop.js
@@ -0,0 +1,25 @@
+
+ // while loop is an entry control loop
+let i =0;
+while (i <=10) {
+    // console.log(i);
+    i++;
+}
+
+let myArray = ["jyoti","preeti","priya","neha"];
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr++;
+}
+
+
+
+// do while loop is an exit control loop
+
+let index = 11;
+do {
+    console.log(`the value of index is ${index}`);
+    index++;
+} while (index <= 10);
+
