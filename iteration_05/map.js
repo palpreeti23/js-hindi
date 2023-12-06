0289da9e4dbb0521ff20a744534e03c94c25c48d
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2023-12-06 08:36:00 +0000

    map done

--- iteration_05/map.js
+++ iteration_05/map.js
@@ -3,6 +3,11 @@ const myArray = [1,2,3,4,5,6,7,8,9,10];
 // console.log(newArray);
 // myArray.forEach( (item)=> )
 
+
+// Take an array of numbers and make them strings
+const newArray = myArray.map( (item )=> item.toString())
+// console.log(newArray);
+
 const newArr = myArray
                .map( (item) => item*10)
                .map( (item) => item +5 )
@@ -10,7 +15,7 @@ const newArr = myArray
                         
 // console.log(newArr)
 
-
+// Capitalize each of an array of names
 const arr = ["preeti","pri","neha","sakshi","ramkumar","rajbaipal","aniruddha"];
 
   const newArr2 = arr.map( (item) => {
@@ -45,9 +50,12 @@ const arr = ["preeti","pri","neha","sakshi","ramkumar","rajbaipal","aniruddha"];
 }
 ];
 
+// Make an array of strings of the names
+
 const newMyArray = myArray2.map( (item) => item.name)
 // console.log(newMyArray)
 
+//Make an array of strings of the names saying whether or not they can go to The Matrix
 const myAnotherArray = myArray2.filter( (item)=> item.age > 20).map( (item) => item.name)
 // console.log(`${myAnotherArray} can go to the matrix `)
-console.log(`${myAnotherArray} can go to the matrix`)
+// console.log(`${myAnotherArray} can go to the matrix`)
