ae65dc550c1f568f55c2fce9105d99465dc17ba7
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2024-03-28 10:43:46 +0000

    for of loop done

--- iteration_05/forOf.js
+++ iteration_05/forOf.js
@@ -12,6 +12,9 @@ for (const arr of myArr) {
     // console.log(arr);
 }
 
+
+//  Maps are particularly advantageous when dealing with large datasets that require frequent changes than objects
+
 let map = new Map();
 map.set('IN',"india");
 map.set('USA',"united states of amerika");
@@ -19,16 +22,19 @@ map.set('FR',"france");
 
 // console.log(map);
 for (const [key,value] of map) {
-    // console.log(key,"=>",value);
+    console.log(key,"=>",value);
 }
 
 
-// for of loop does not work on objects.
+// for of loop work better on objects.
+
+// in objects we cant user object name directly we use object.entries(objname)
+
 const myObj = {
-    'name1':"preeti",
-    'name2':"priya"
+    name1:"preeti",
+    age : 19
 }
 
-for (const value of myObj) {
-    console.log(value);
+for (const [key, value] of Object.entries(myObj)) {
+    console.log(`${key}:${value}`)
 }
