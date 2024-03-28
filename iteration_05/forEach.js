0cc850a32dc194a844e035b9677cfcaaaf02e37e
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2024-03-28 10:04:25 +0000

    for each loop done

--- iteration_05/forEach.js
+++ iteration_05/forEach.js
@@ -1,21 +1,22 @@
 
 const myArray = [2,4,6,8,9];
 
-myArray.forEach( function(value) {
-    // console.log(value);
+// myArray.forEach( function(value) {
+//     // console.log(value);
 
-})
+// })
+
 
 // we cant store forEach loop in any other variable. it gives undefined 
 const newArray = myArray.forEach( (value) => { return(value*10)})
-console.log(newArray)
+// console.log(newArray)
 
 myArray.forEach( (item)=>{
     // console.log(item);
 })
 
 function print(value,index,arr){
-    console.log(value,index,arr)
+    // console.log(value,index,arr)
 }
 
 // myArray.forEach(print);
@@ -44,7 +45,7 @@ language.forEach( (key) => {
 })
 
 
-// for each loop does not work for objects
+// forEach loop does not work for objects
 let myObj = {
     name:"preeti",
     age:20,
@@ -53,4 +54,38 @@ let myObj = {
 
 // myObj.forEach( (key)=>{
 //     // console.log(Object.key);
-//})
+// })
+
+// Object.keys(myObj).forEach((key)=>{
+
+//     console.log(key)
+//     console.log(myObj[key])
+// })
+
+let arr = [1,2,3,4,5,9]
+
+arr.forEach( function(value){
+    console.log(value);
+})
+
+const obj = {
+    fname:'preeti',
+    age:20,
+    clg:'chouksey'
+}
+
+
+// Object.keys gives you an array of the keys in that object.
+// where as Object.entries returns an array of arrays containing key value pair.
+
+Object.keys(obj).forEach( function(value){
+    console.log(`${value} : ${obj[value]}`);
+    // console.log(obj[value])
+})
+
+Object.entries(obj).forEach( function(value){
+    // console.log(`${value} : ${obj[value]}`);
+    console.log(value)
+})
+
+
